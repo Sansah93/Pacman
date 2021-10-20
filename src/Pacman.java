7378045440f8bee175ee23648c7c42b53941a24d
mialy mialy <mialysandy@gmail.com>
2021-10-20 13:02:58 +0200

    4 fantome fonctionnant mais code bien moche sa mère

--- src/Pacman.java
+++ src/Pacman.java
@@ -8,15 +8,18 @@
 	
 	public class Pacman	extends JPanel{
 		
-		private int x ,y,velY,velX,score,cpt = 0,cpt2 = 0,rndm;
+		private int x ,y,velY,velX,score,cpt = 0,cpt2 = 0,rndm,rndm2,rndm3,rndm4;
 		private final int vel=50;
 		private  int map[][];
-		private char moves,movesG;
+		private char moves,movesG1 = 'a',movesG2,movesG3,movesG4;
 		private Color couleur = Color.YELLOW;
 		private  int life;
 		private boolean jauneP,orange;
 		
 		Ghost ghost1= new Ghost(true);
+		Ghost ghost2= new Ghost(true);
+		Ghost ghost3= new Ghost(true);
+		Ghost ghost4= new Ghost(true);
 		public void setXY(int x, int y) {
 			this.x=x;
 			this.y=y;
@@ -44,7 +47,19 @@
 				
 		ghost1.setPosX(200);
 		ghost1.setPosY(200);
-		rndmGhost();
+		rndmGhost(ghost1);
+
+		ghost2.setPosX(300);
+		ghost2.setPosY(300);
+		rndmGhost2(ghost2);
+
+		ghost3.setPosX(400);
+		ghost3.setPosY(400);
+		rndmGhost3(ghost3);
+
+		ghost4.setPosX(500);
+		ghost4.setPosY(500);
+		rndmGhost4(ghost4);
 		}
 		
 		
@@ -55,6 +70,7 @@
 			
 			int height = 700;
 			super.paint(g);
+			System.out.println(movesG1);
 			/*g.drawLine(width-5, 5, width-5, height-5);
 			g.drawLine(5, 5, 5, height-5);
 			g.drawLine(5,5,width-5,5);
@@ -93,12 +109,24 @@
 				g.setColor(Color.cyan);
 				g.fillRect(ghost1.getPosX(), ghost1.getPosY(), vel, vel);
 			}
-			
+			if(ghost2.isAlive()) {
+				g.setColor(Color.RED);
+				g.fillRect(ghost2.getPosX(), ghost2.getPosY(), vel, vel);
+			}
+			if(ghost3.isAlive()) {
+				g.setColor(Color.GRAY);
+				g.fillRect(ghost3.getPosX(), ghost3.getPosY(), vel, vel);
+			}
+			if(ghost4.isAlive()) {
+				g.setColor(Color.GREEN);
+				g.fillRect(ghost4.getPosX(), ghost4.getPosY(), vel, vel);
+			}
 		  	g.setColor(couleur);
 		  	
 		  	//g.drawRect(x,y,50,50);
 		  	System.out.println(x + " " + y+ " score : " + score);
 		  	g.fillRect(x, y, 50, 50);
+		  	
 		  	
 		}
 		
@@ -281,47 +309,126 @@ this.map= new int[][]{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		
 		//methodes pour les fantomes
 		
-		
+
+		//il faut mettre le vel du ghost (ghost.getVel())
+		public boolean checkCollG(int x,int y,char movesG) {
+			switch(movesG) {
+			case 'z':
+				if(map[(x/vel)-1][(y/vel)-2] == 1){
+					
+					return true;
+				}
+				
+				break;
+			case 'q':
+				if(map[(x/vel)-2][(y/vel)-1] == 1){
+					
+					return true;
+				}
+				
+				break;
+			case 's':
+				if(map[(x/vel-1)][(y/vel-1+1)] == 1){
+					
+					return true;
+				}
+				
+				break;
+			case 'd':
+				if(map[(x/vel)][(y/vel)-1] == 1){
+					
+					return true;
+				}
+				
+				break;
+			
+			
+			}
+			return false;
+		}
 		
 		
 		ActionListener actionG = new ActionListener() {
 			public void actionPerformed(ActionEvent event) {	
-				if(!checkCollisions(ghost1.getPosX(),ghost1.getPosY(),movesG)) {
+				if(!checkCollisions(ghost1.getPosX(),ghost1.getPosY(),movesG1)) {
 				ghost1.setPosX(ghost1.getPosX()+ ghost1.getVelX());
 				ghost1.setPosY(ghost1.getPosY()+ghost1.getVelY()); 
 				
 				if(rndm == 10) {
-					rndmGhost();
+					rndmGhost(ghost1);
 					rndm = 0;
 				}
 				rndm++;
 				repaint(); 
 				}else {
-					rndmGhost();
+					rndmGhost(ghost1);
 				}
+				if(!checkCollisions(ghost2.getPosX(),ghost2.getPosY(),movesG2)) {
+					ghost2.setPosX(ghost2.getPosX()+ ghost2.getVelX());
+					ghost2.setPosY(ghost2.getPosY()+ghost2.getVelY()); 
+					
+					if(rndm2 == 10) {
+						rndmGhost2(ghost2);
+						rndm2 = 0;
+					}
+					rndm2++;
+					repaint(); 
+					}else {
+						rndmGhost2(ghost2);
+					}
+				if(!checkCollisions(ghost3.getPosX(),ghost3.getPosY(),movesG3)) {
+					ghost3.setPosX(ghost3.getPosX()+ ghost3.getVelX());
+					ghost3.setPosY(ghost3.getPosY()+ghost3.getVelY()); 
+					
+					if(rndm3 == 10) {
+						rndmGhost3(ghost3);
+						rndm3 = 0;
+					}
+					rndm3++;
+					repaint(); 
+					}else {
+						rndmGhost3(ghost3);
+					}
+				if(!checkCollisions(ghost4.getPosX(),ghost4.getPosY(),movesG4)) {
+					ghost4.setPosX(ghost4.getPosX()+ ghost4.getVelX());
+					ghost4.setPosY(ghost4.getPosY()+ghost4.getVelY()); 
+					
+					if(rndm4 == 10) {
+						rndmGhost4(ghost4);
+						rndm4 = 0;
+					}
+					rndm4++;
+					repaint(); 
+					}else {
+						rndmGhost4(ghost4);
+					}
 			}
 		};
+		
+		
+		
+		
 		Timer tG = new Timer(ghost1.getVel()*5,actionG);
 		
-		public void moveG(char move) {
+		public void moveG(char move,Ghost ghost) {
 			switch(move) {
-			case 'z' :ghost1.setVelX(0);;
-					  ghost1.setVelY(-ghost1.getVel());
+			case 'z' :ghost.setVelX(0);;
+					  ghost.setVelY(-ghost.getVel());
 					  
 					  tG.start();
 			break;
-			case 's' : ghost1.setVelX(0);
-					   ghost1.setVelY(ghost1.getVel());
+			case 's' : ghost.setVelX(0);
+					   ghost.setVelY(ghost.getVel());
 					  
 			tG.start();
 			break;
-			case 'q' : ghost1.setVelX(-ghost1.getVel());
-			           ghost1.setVelY(0);
+			case 'q' : ghost.setVelX(-ghost.getVel());
+			           ghost.setVelY(0);
 			           
 			           tG.start();
 			break;
-			case 'd' :ghost1.setVelX(ghost1.getVel());
-					  ghost1.setVelY(0);
+			case 'd' :ghost.setVelX(ghost.getVel());
+					  ghost.setVelY(0);
 					  tG.start();		  
 			
 			break;				 
@@ -329,17 +436,55 @@ this.map= new int[][]{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 			}
 		}
 		
-		public void rndmGhost() {
+		public void rndmGhost(Ghost ghost) {
 			int r = (int) (Math.random()*4);
 			switch(r) {
-			case  0 :moveG('z');movesG = 'z';break;
+			case  0 :moveG('z',ghost);movesG1 = 'z';break;
 
-			case  1 :moveG('s');movesG = 's';break;
-			case  2 :moveG('q');movesG = 'q';break;
-			case  3 :moveG('d');movesG = 'd';break;
+			case  1 :moveG('s',ghost);movesG1 = 's';break;
+			case  2 :moveG('q',ghost);movesG1 = 'q';break;
+			case  3 :moveG('d',ghost);movesG1 = 'd';break;
 			
 			}
+			
 		}
-	}
+		public void rndmGhost2(Ghost ghost) {
+			int r = (int) (Math.random()*4);
+			switch(r) {
+			case  0 :moveG('z',ghost);movesG2 = 'z';break;
+
+			case  1 :moveG('s',ghost);movesG2 = 's';break;
+			case  2 :moveG('q',ghost);movesG2 = 'q';break;
+			case  3 :moveG('d',ghost);movesG2 = 'd';break;
+			
+			}
+			
+		}
+		public void rndmGhost3(Ghost ghost) {
+			int r = (int) (Math.random()*4);
+			switch(r) {
+			case  0 :moveG('z',ghost);movesG3 = 'z';break;
+
+			case  1 :moveG('s',ghost);movesG3 = 's';break;
+			case  2 :moveG('q',ghost);movesG3 = 'q';break;
+			case  3 :moveG('d',ghost);movesG3 = 'd';break;
+			
+			}
+			
+		}
+		public void rndmGhost4(Ghost ghost) {
+			int r = (int) (Math.random()*4);
+			switch(r) {
+			case  0 :moveG('z',ghost);movesG4 = 'z';break;
+
+			case  1 :moveG('s',ghost);movesG4 = 's';break;
+			case  2 :moveG('q',ghost);movesG4 = 'q';break;
+			case  3 :moveG('d',ghost);movesG4 = 'd';break;
+			
+			}
+			
+		}
+		}
+	
 	
 	
